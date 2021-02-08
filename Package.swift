a20934c6c667e1faa2c229509ca53a4be8b72b87
Hans Terje Bakke <hans.terje.bakke@gmail.com>
2021-02-08 14:08:58 +0100

    Dependency on multicast package from local to github

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
         .library(name: "ITLogger", targets: ["ITLogger"]),
     ],
     dependencies: [
-        .package(path: "../ITMulticastDelegate")
+        //.package(path: "../ITMulticastDelegate")
+        .package(url: "git@github.com:htb/ITMulticastDelegate.git", from: "1.0.0")
     ],
     targets: [
         .target(name: "ITLogger", dependencies: []),
